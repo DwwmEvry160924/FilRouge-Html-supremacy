1e01f58c33f65b496a27eabe6bd13a5d210bc8b9
salym da costa <salymdc@gmail.com>
2024-11-26 10:27:54 +0100

    MAJ[SD]

--- assets/js/script_chef_de_projet.js
+++ assets/js/script_chef_de_projet.js
@@ -1,7 +1,7 @@
-// function popupNotif() {
-//     let popup = document.getElementById('Notification');
-//     popup.classList.toggle('show')
-// }
+function popupNotif() {
+    let popup = document.getElementById('Notification');
+    popup.classList.toggle('show')
+}
 
 
 const dropdownButton = document.querySelector('.dropdown-btn button');
@@ -18,92 +18,89 @@ let projet = document.querySelectorAll('.event')
 
 // let card = document.querySelectorAll('.event')
 
-// // dropdownButton.addEventListener('click', () => {
-// //   dropdownContent.classList.toggle('show');
-// // });
+dropdownButton.addEventListener('click', () => {
+  dropdownContent.classList.toggle('show');
+});
 
-// // dropdownButtonFiltre.addEventListener('click', () => {
-// //   dropdownContentFiltre.classList.toggle('show');
-// // });
-
-
+dropdownButtonFiltre.addEventListener('click', () => {
+  dropdownContentFiltre.classList.toggle('show');
+});
 
 
-// dropdownContent.addEventListener('click', (event) => {
-//   let option = event.target.textContent;
-//   console.log(option)
-//   switch (option) {
-//     case 'Jour':
-//       days[0].style.display = 'block'; 
-//       for(let i = 1; i <= 4; i++){
-//         days[i].style.display = 'none'
-//       }
-//       break;
 
-//     case '3 Jours':
-//       days[0,1,2].style.display = 'block'; 
-//       for(let i = 3; i <= 4; i++){
-//         days[i].style.display = 'none'
-//       }
-//       break;
 
-//     case 'Semaine':
-//       for(let i = 0; i <= 4; i++){
-//         days[i].style.display = 'block'
-//       }
-//       break;
-//   }
-// });
+dropdownContent.addEventListener('click', (event) => {
+  let option = event.target.textContent;
+  console.log(option)
+  switch (option) {
+    case 'Jour':
+      days[0].style.display = 'block'; 
+      for(let i = 1; i <= 4; i++){
+        days[i].style.display = 'none'
+      }
+      break;
 
-// dropdownContentFiltre.addEventListener('click',(e) => {
-//   let op = e.target.textContent;
-//   console.log(op)
-//   switch(op){
-//     case 'en cours':
-//     for (let i = 0; i < enCours.length; i++) {
-//         enCours[i].style.display = 'block';
-//     } 
-//     for(let i = 0; i <= 4;i++){
-//       enRetard[i].style.display = 'none'
-//       termine[i].style.display = 'none'
-//     }
-//     break;
+    case '3 Jours':
+      days[0,1,2].style.display = 'block'; 
+      for(let i = 3; i <= 4; i++){
+        days[i].style.display = 'none'
+      }
+      break;
 
-//     case 'terminés': 
-//     for (let i = 0; i < termine.length; i++) {
-//       termine[i].style.display = 'block';
-//   }
-//     for(let i = 0; i <= 4;i++){
-//       enCours[i].style.display ='none'
-//       enRetard[i].style.display = 'none'
-//     }
-//     break;
+    case 'Semaine':
+      for(let i = 0; i <= 4; i++){
+        days[i].style.display = 'block'
+      }
+      break;
+  }
+});
 
-//     case 'en retard':
-//     for (let i = 0; i < enRetard.length; i++) {
-//         enRetard[i].style.display = 'block';
-//     }
-//     for(let i = 0; i <= 5;i++){
-//       enCours[i].style.display ='none'
-//       termine[i].style.display = 'none'
-//     }
-//     break;
-//     case 'tout':
-//     for (let i = 0; i < projet.length; i++) {
-//         projet[i].style.display = 'block';
-//     }
+dropdownContentFiltre.addEventListener('click',(e) => {
+  let op = e.target.textContent;
+  console.log(op)
+  switch(op){
+    case 'en cours':
+    for (let i = 0; i < enCours.length; i++) {
+        enCours[i].style.display = 'block';
+    } 
+    for(let i = 0; i <= 4;i++){
+      enRetard[i].style.display = 'none'
+      termine[i].style.display = 'none'
+    }
+    break;
+
+    case 'terminés': 
+    for (let i = 0; i < termine.length; i++) {
+      termine[i].style.display = 'block';
+  }
+    for(let i = 0; i <= 4;i++){
+      enCours[i].style.display ='none'
+      enRetard[i].style.display = 'none'
+    }
+    break;
+
+    case 'en retard':
+    for (let i = 0; i < enRetard.length; i++) {
+        enRetard[i].style.display = 'block';
+    }
+    for(let i = 0; i <= 5;i++){
+      enCours[i].style.display ='none'
+      termine[i].style.display = 'none'
+    }
+    break;
+    case 'tout':
+    for (let i = 0; i < projet.length; i++) {
+        projet[i].style.display = 'block';
+    }
     
-//   }
-// });
+  }
+});
 
-// console.log(projet)
-// console.log(enCours)
-// console.log(enRetard)
-// console.log(termine)
+console.log(projet)
+console.log(enCours)
+console.log(enRetard)
+console.log(termine)
 
-// console.log(days)
-
-// creation de projet 
-
+console.log(days)
 
 
